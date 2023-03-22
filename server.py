226a7ed53f59762a6d80ed5fbdea8fa84b36252b
Chloe Sim <chloesim@MacBook-Pro-3.smc.local>
2023-03-22 14:27:46 +1300

    added resource and news pages and also made a parallax identity

--- server.py
+++ server.py
@@ -12,11 +12,11 @@ def competitions():
 
 @app . route ('/resources')
 def resources():
-    return "<h1>resources</h1>"
+    return render_template("resources.html")
 
 @app . route ('/news')
 def news():
-    return "<h1>news</h1>"
+    return render_template("news.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
