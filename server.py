65cf29a1b4327d2fc825e180296f3a663dba1b4a
Chloe Sim <chloesim@MacBook-Pro-3.local>
2023-05-09 12:35:00 +1200

    added news cud page, set up form page and made error page

--- server.py
+++ server.py
@@ -11,19 +11,19 @@ def news_date(sqlite_dt):
     x = datetime.strptime(sqlite_dt, '%Y-%m-%d %H:%M:%S')
     return x.strftime("%a %d %b %Y %H:%M")
 
-@app . route ('/')
+@app.route ('/')
 def index():
     return render_template("index.html")
 
-@app . route ('/competitions')
+@app.route ('/competitions')
 def competitions():
     return render_template("competitions.html")
 
-@app . route ('/resources')
+@app.route ('/resources')
 def resources():
     return render_template("resources.html")
 
-@app . route ('/news')
+@app.route ('/news')
 def news():
     # query for the page
     sql = """select news.news_id, news.title, news.subtitle, news.content, news.newsdate, member.name
@@ -34,6 +34,29 @@ def news():
     result = run_search_query_tuples(sql, (), db_path, True)
     print(result)
     return render_template("news.html", news=result)
+
+@app.route ('/news_cud', methods=['GET', 'POST'])
+def news_cud():
+    #collect data from the web address
+    data = request.args
+    required_keys = ['id','task']
+    for k in required_keys:
+        if k not in data.keys():
+            message = "Do not know what to do with read update on news (key not present)"
+            return render_template('error.html', message=message)
+    # have an ID and a task key
+    if request.method == "GET":
+        if data['task'] == 'delete':
+            return "<h1> I want to delete </h1>"
+        elif data['task'] == 'update':
+            return "<h1> I want to update </h1>"
+        elif data['task'] == 'add':
+            return "<h1> I want to update </h1>"
+        else:
+            message = "Unrecognised task coming from news page"
+            return render_template('error.html', message=message)
+
+    return render_template("news_cud.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
