3d780204da3362d6ba54ed18ac9abd563ee6d4a9
Chloe Sim <chloesim@MacBook-Pro-3.local>
2023-06-03 12:41:30 +1200

    added a signup form and created a confirm page

--- server.py
+++ server.py
@@ -124,23 +124,24 @@ def logout():
 
 @app.route('/signup', methods=["GET", "POST"])
 def signup():
+    referrer = request.referrer
+    print(referrer)
     if request.method == "POST":
         f = request.form
         return render_template("confirm.html", form_data=f)
     elif request.method == "GET":
-        carried_data = request_args
+        carried_data = request.args
         print(carried_data)
         if len(carried_data) == 0:
             temp_form_data = {
                 "firstname": "James",
-                "secondname": "Lovelock",
+                "lastname": "Lovelock",
                 "email": "jl@gmail.com",
                 "aboutme": "I have been in love with Italian food all my life"
             }
-            # temp_form_data = {}
         else:
             temp_form_data = carried_data
-        return render_template("signup.html", **temp_form_data)
+    return render_template("signup.html", **temp_form_data)
 
 
 if __name__ == "__main__":
