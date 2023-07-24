8b213810365619cbc208f61b1cfd62caa17806f8
Chloe Sim <chloesim@MacBook-Pro-3.smc.local>
2023-07-24 13:19:32 +1200

    did tests

--- testing.py
+++ testing.py
@@ -1,26 +1,29 @@
 from db_functions import run_search_query_tuples
 
 def get_comments(db_path):
+    sql="""select news.news_id,news.title,news.subtitle, news.content
+     from news
+     order by news.newsdate desc
+     """
+    news_items = run_search_query_tuples(sql, (), db_path, True)
     sql="""select news.news_id,news.title,news.subtitle, news.content, comment.comment, member.firstname
      from news
      left join comment on news.news_id = comment.news_id
      left join member on comment.member_id = member.member_id
      order by news.newsdate desc
      """
-    result = run_search_query_tuples(sql, (), db_path, True)
+    comments = run_search_query_tuples(sql, (), db_path, True)
+    for row in news_items:
+        print(row['title'])
+        for com in comments:
+            if com['news_id'] == row['news_id']:
+                print(com['comment'])
 
-    newsID = 0
 
-    for row in result:
-        if row['news_id'] != newsID:
-            news_item = "{} {} {}".format(row['news_id'], row['title'], row['subtitle'])
-            print(news_item)
-            comment = "{} {}".format(row['comment'], row['firstname'])
-            print(comment)
-            newsID = row['news_id']
-        else:
-            comment = "{} {}".format(row['comment'], row['firstname'])
-            print(comment)
+
+
+
+
 
 
 
