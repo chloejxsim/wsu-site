35ba16c9f29a0ba0ea506ff402f80c175b1e3d7c
Chloe Sim <chloesim@Chloes-MBP.smc.local>
2023-08-30 10:34:27 +1200

    did testing

--- testing.py
+++ testing.py
@@ -19,7 +19,43 @@ def get_comments(db_path):
             if com['news_id'] == row['news_id']:
                 print(com['comment'])
 
+def get_news(db_path):
+    sql = """select news.title, news.subtitle, news.content, member.firstname
+        from news
+        join member on news.member_id = member.member_id;
+    """
+    result = run_search_query_tuples(sql, (), db_path, True)
+
+    for row in result:
+        for k in row.keys():
+            print(k)
+            print(row[k])
+
+def get_schedule(db_path):
+    sql = """select schedule.event, schedule.description, schedule.scheduledate, schedule.location
+        from schedule
+        join member on schedule.member_id = member.member_id;
+    """
+    result = run_search_query_tuples(sql, (), db_path, True)
+
+    for row in result:
+     for k in row.keys():
+        print(k)
+        print(row[k])
+
+def get_draw(db_path):
+    sql = """select draw_id, grade, round, affirming, negating, winner from draw where grade='Prem A'"""
+    result = run_search_query_tuples(sql, (), db_path, True)
+
+    for row in result:
+     for k in row.keys():
+        print(k)
+        print(row[k])
 
 if __name__ == "__main__":
     db_path = 'data/wsusite_db.sqlite'
-    get_comments(db_path)
+    #get_news(db_path)
+    #get_schedule(db_path)
+    get_draw(db_path)
+    #get_comments(db_path)
+
